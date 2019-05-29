f901d79ba2b86936d1823fbcc36ba04ddd1a91ff
ymy1248 <ymy1248@mac.local>
2019-05-29 23:53:31 +0800

    finished effective c++ item 51

--- effective_c++/51_new_delete.cpp
+++ effective_c++/51_new_delete.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void* operator new(std::size_t size) throw (std::bad_alloc) 
 {
-  cout << "customized operator new, size: " << size << endl;
+  cout << "customized global operator new, size: " << size << endl;
 
   void *p;
   if (size ==  0) {
@@ -23,17 +23,35 @@ void* operator new(std::size_t size) throw (std::bad_alloc)
   }
 }
 
+void operator delete(void *rawMemory) throw()
+{
+  cout << "customized global operator delete" << endl;
+  if (!rawMemory) return;
+  free(rawMemory);
+}
+
 class A {
   int num;
 };
 
 class Base {
 public:
-  static void* operator new(std::size_t size) throw(std::bad_alloc) {
+  static void* operator new(std::size_t size) throw(std::bad_alloc)
+  {
     if (size != sizeof(Base))
       return ::operator new(size);
     cout << "Base::new" << endl;
     return ::operator new(size);
+  }
+
+  static void operator delete(void* rawMemory, size_t size) throw() // size_t is necessary
+  {
+    cout << "cutomized member operator delete" << endl;
+    if (rawMemory == 0) return;
+    if (size != sizeof(Base)) {       // check memory size
+      ::operator delete(rawMemory);
+      return;
+    }
   }
 
 private:
@@ -51,10 +69,13 @@ private:
 int main() {
   cout << "Test global new" << endl;
   A *a = new A();
+  delete a;
 
   cout << endl << "Test member new" << endl;
   Base *b = new Base();
+  delete b;
 
   cout << endl << "Test deried" << endl;
   Derived *d = new Derived();
+  delete d;
 }
